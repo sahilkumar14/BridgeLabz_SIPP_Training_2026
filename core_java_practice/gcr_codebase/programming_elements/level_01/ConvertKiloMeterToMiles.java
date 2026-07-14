11cbd3ef42dc8e06b17ddc4e335f15b8c6fbdcc8
Sahil.Kumar <158182813+sahilkumar14@users.noreply.github.com>
2026-07-14 13:33:00 +0530

    Delete core_java_practice/gcr_codebase/programming_elements directory

--- core_java_practice/gcr_codebase/programming_elements/level_01/ConvertKiloMeterToMiles.java
+++
@@ -1,9 +0,0 @@
-package BridgeLabz_SIPP_Training_2026.core_java_practice.gcr_codebase.programming_elements.level_01;
-public class ConvertKiloMeterToMiles {
-    public static void main(String[] args) {
-        double distanceInKilometer = 10.8;
-        double convertedToMiles = distanceInKilometer * 1.6;
-
-        System.out.print("the distance "+distanceInKilometer+"km in miles is "+convertedToMiles);
-    }
-}
