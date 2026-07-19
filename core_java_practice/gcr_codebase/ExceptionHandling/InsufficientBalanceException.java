9203b25e64744f2874cacd6dd1c926a0b8fc330c
sahilkumar <sahilkv14@gmail.com>
2026-07-19 16:48:59 +0530

    [Sahil].Add Added exercise solution for javaIO and exception handling

---
+++ core_java_practice/gcr_codebase/ExceptionHandling/InsufficientBalanceException.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class InsufficientBalanceException
+        extends Exception {
+
+    public InsufficientBalanceException(
+            String message) {
+
+        super(message);
+    }
+}
---
+++ core_java_practice/gcr_codebase/ExceptionHandling/TryWithResources.java
@@ -0,0 +1,23 @@
+package ExceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br =
+                     new BufferedReader(
+                             new FileReader("info.txt"))) {
+
+            System.out.println(br.readLine());
+
+        }
+
+        catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
